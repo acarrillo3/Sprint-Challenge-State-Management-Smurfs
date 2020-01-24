1af0044fe1a37f7c41ddce718098c89c7ad13c4b
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-24 14:05:35 -0500

    addded styles

--- smurfs/src/components/Card.js
+++ smurfs/src/components/Card.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Card = (props) => {
-    const { smurf } = props;
+const Card = props => {
+  const { smurf } = props;
 
-    return (
-        <div className="card">
-            <h2>{smurf.name}</h2>
-            <p>{smurf.age}</p>
-            <p>{smurf.height}</p>
-        </div>
-    )
-}
+  return (
+    <div className="card">
+      <h2>{smurf.name}</h2>
+      <p>Age: {smurf.age} years old</p>
+      <p>Height: {smurf.height} tall</p>
+    </div>
+  );
+};
 
-export default Card; 
+export default Card;
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,31 +1,31 @@
-import React, {useEffect} from 'react'
-import { connect } from 'react-redux';
-import {fetchSmurfs} from '../actions';
-import Card from './Card';
-import AddForm from './AddForm';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchSmurfs } from "../actions";
+import Card from "./Card";
+import AddForm from "./AddForm";
 
-const SmurfsList = (props) => {
-    const {smurfs} = props;
-    // Fetch smurfs data on load.
-    useEffect(() => {
-        props.fetchSmurfs();
-    }, [])
+const SmurfsList = props => {
+  const { smurfs } = props;
+  // Fetch smurfs data on load.
+  useEffect(() => {
+    props.fetchSmurfs();
+  }, []);
 
-    return (
-        <div>
-            <h1>Welcome to Smurfs Town, Register!</h1>
-            <AddForm />
-            {smurfs.map((smurf => (
-                <Card key={smurf.id} smurf={smurf} />
-            )))}
-        </div>
-    )
-}
-
-const mapStateToProps = (state) => {
-    return {
-        smurfs: state.smurfs
-    }
+  return (
+    <div className="smurfs-list">
+      <h1>Register: </h1>
+      <AddForm />
+      {smurfs.map(smurf => (
+        <Card key={smurf.id} smurf={smurf} />
+      ))}
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, {fetchSmurfs})(SmurfsList);
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs
+  };
+};
+
+export default connect(mapStateToProps, { fetchSmurfs })(SmurfsList);
